b2498804b21c8cb3dfb707dbc0c47f5ad860efa1
zzlev <gpalektzja@gmail.com>
2022-12-20 09:37:24 +0900

    Add WebConfig.

---
+++ src/main/java/hello/login/WebConfig.java
@@ -0,0 +1,22 @@
+package hello.login;
+
+import hello.login.web.filter.LoginCheckFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.servlet.Filter;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public FilterRegistrationBean<Filter> logFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
+    }
+}
